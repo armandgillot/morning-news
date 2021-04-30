ee6882f604571f37c0d69795e29cad24fe0204ed
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-04-30 16:10:29 +0200

    route articles_saved created in backend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var request = require("sync-request");
+var usersModel = require("../models/usersSchema");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,18 +9,29 @@ router.get("/", function (req, res, next) {
 });
 
 /* API NEWS SOURCES */
-router.get("/news", function (req, res, next) {
+router.get("/news", async (req, res, next) => {
+  var user = await usersModel.findOne({ token: req.query.token });
+
   var language = "fr";
-  if (req.query.country === "gb") {
+  if (user.language === "gb") {
     language = "en";
   }
   var requete = request(
     "GET",
-    `https://newsapi.org/v2/sources?language=${language}&country=${req.query.country}&apiKey=${process.env.API_KEY}`
+    `https://newsapi.org/v2/sources?language=${language}&country=${user.language}&apiKey=${process.env.API_KEY}`
   );
   var result = JSON.parse(requete.body);
 
   res.json({ result: result.sources });
+});
+
+router.put("/news", async (req, res, next) => {
+  var user = await usersModel.findOne({ token: req.query.token });
+  console.log(user.language);
+  console.log(req.query.country);
+  user.language = req.query.country;
+  await user.save();
+  res.json(user);
 });
 
 /* ARTICLE BY SOURCE */
@@ -32,6 +44,13 @@ router.get("/news-bysource", function (req, res, next) {
 
   res.json({ result: result.articles });
 });
+
+/* Change Country */
+// router.get("/changeCountry", async (req, res, next) => {
+//   var user = await usersModel.findOne({ token: req.query.token });
+//   var language = user.language;
+//   res.json({ language });
+// });
 
 module.exports = router;
 
