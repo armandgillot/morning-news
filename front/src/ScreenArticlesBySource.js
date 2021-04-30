1ed241ba2a6d5f9151216cddf775de1f6b6b177b
ramos1759 <hermann.kuate@epitech.eu>
2021-04-30 16:28:30 +0200

    envoi au back article

--- front/src/ScreenArticlesBySource.js
+++ front/src/ScreenArticlesBySource.js
@@ -1,112 +1,145 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import { Card, Icon, Modal } from "antd";
+import {Card, Icon, Modal} from "antd";
 import Nav from "./Nav";
-import { useParams } from "react-router-dom";
-import { connect } from "react-redux";
+import {useParams} from "react-router-dom";
+import {connect} from "react-redux";
 
-const { Meta } = Card;
+const {Meta} = Card;
 
 function ScreenArticlesBySource(props) {
-  const [sourceList, setSourceList] = useState([]);
+    const [sourceList, setSourceList] = useState([]);
 
-  const [visible, setVisible] = useState(false);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
-  console.log(props.token);
-  console.log(sourceList);
+    // console.log(props.token);
+    // console.log(sourceList);
 
-  var { id } = useParams();
+    var {id} = useParams();
 
-  useEffect(() => {
-    const APIResultsLoading = async () => {
-      const result = await fetch(`/news-bysource?id=${id}`);
-      const body = await result.json();
-      setSourceList(body.result);
+    useEffect(() => {
+        const APIResultsLoading = async () => {
+            const result = await fetch(`/news-bysource?id=${id}`);
+            const body = await result.json();
+            setSourceList(body.result);
+        };
+        APIResultsLoading();
+    }, []);
+
+    var showModal = (title, content) => {
+        setVisible(true);
+        setTitle(title);
+        setContent(content);
     };
-    APIResultsLoading();
-  }, []);
 
-  var showModal = (title, content) => {
-    setVisible(true);
-    setTitle(title);
-    setContent(content);
-  };
+    var handleOk = (e) => {
+        console.log(e);
+        setVisible(false);
+    };
 
-  var handleOk = (e) => {
-    console.log(e);
-    setVisible(false);
-  };
 
-  var handleCancel = (e) => {
-    console.log(e);
-    setVisible(false);
-  };
 
-  return (
-    <div>
-      <Nav />
+ //   const savebdd = (article) => {
 
-      <div className="Banner" />
+        //    props.addToWishList(article);}
 
-      <div className="Card">
-        {sourceList.map((article, i) => (
-          <div key={i} style={{ display: "flex", justifyContent: "center" }}>
-            <Card
-              style={{
-                width: 300,
-                margin: "15px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-              }}
-              cover={<img alt="example" src={article.urlToImage} />}
-              actions={[
-                <Icon
-                  type="read"
-                  key="ellipsis2"
-                  onClick={() => showModal(article.title, article.content)}
-                />,
-                <Icon
-                  type="like"
-                  key="ellipsis"
-                  onClick={() => {
-                    props.addToWishList(article);
-                  }}
-                />,
-              ]}
-            >
-              <Meta title={article.title} description={article.description} />
-            </Card>
+    const savebdd = async (article) => {
+        const articleSend = await fetch('/articles_saved', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: `content=${article.content}`
+        });
+        console.log(articleSend)
+    }
 
-            <Modal
-              title={title}
-              visible={visible}
-              onOk={handleOk}
-              onCancel={handleCancel}
-            >
-              <p>{content}</p>
-            </Modal>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+
+
+    /*
+    useEffect( async () => {
+        const articleSend = await fetch('/articles_saved', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: 'content=Test&description=TexteDescription'
+        });
+        console.log(articleSend)
+    }, []);
+
+
+     */
+
+
+    var handleCancel = (e) => {
+        console.log(e);
+        setVisible(false);
+    };
+
+    return (
+        <div>
+            <Nav/>
+
+            <div className="Banner"/>
+
+            <div className="Card">
+                {sourceList.map((article, i) => (
+                    <div key={i} style={{display: "flex", justifyContent: "center"}}>
+                        <Card
+                            style={{
+                                width: 300,
+                                margin: "15px",
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "space-between",
+                            }}
+                            cover={<img alt="example" src={article.urlToImage}/>}
+                            actions={[
+                                <Icon
+                                    type="read"
+                                    key="ellipsis2"
+                                    onClick={() => showModal(article.title, article.content)}
+                                />,
+                                <Icon
+                                    type="like"
+                                    key="ellipsis"
+                                    onClick={() => {
+                                        savebdd(article);
+                                    }}
+                                />,
+                            ]}
+                        >
+                            <Meta title={article.title} description={article.description}/>
+                        </Card>
+
+                        <Modal
+                            title={title}
+                            visible={visible}
+                            onOk={handleOk}
+                            onCancel={handleCancel}
+                        >
+                            <p>{content}</p>
+                        </Modal>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    addToWishList: function (article) {
-      dispatch({ type: "addArticle", articleLiked: article });
-    },
-  };
+    return {
+        addToWishList: function (article) {
+            dispatch({type: "addArticle", articleLiked: article});
+        },
+    };
 }
+
 function mapStateToProps(state) {
-  return { token: state.token };
+    return {token: state.token};
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(ScreenArticlesBySource);
